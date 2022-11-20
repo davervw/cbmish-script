a1a552861f48072d776ac1afa249a453c9bd3a45
Dave Van Wagner <git@davevw.com>
2022-11-20 14:53:38 -0800

    almost working

---
+++ cbmish.ts
@@ -0,0 +1,180 @@
+const C64colors: number[][] = [
+    [0, 0, 0, 255],       // [0] black
+    [255, 255, 255, 255], // [1] white
+    [192, 0, 0, 255],     // [2] red
+    [0, 255, 255, 255],   // [3] cyan
+    [160, 32, 160, 255],  // [4] purple
+    [32, 160, 32, 255],   // [5] green
+    [64, 64, 192, 255],   // [6] blue
+    [255, 255, 128, 255], // [7] yellow
+    [255, 128, 0, 255],   // [8] orange
+    [128, 64, 0, 255],    // [9] brown  
+    [192, 32, 32, 255],   // [10] lt red
+    [64, 64, 64, 255],    // [11] dk gray
+    [128, 128, 128, 255], // [12] med gray
+    [160, 255, 160, 255], // [13] lt green
+    [96, 128, 240, 255],  // [14] lt blue
+    [192, 192, 192, 255], // [15] lt gray
+  ];
+
+class CbmishConsole {
+    dirtyx = 0;
+    dirtyy = 0;
+    dirtywidth = 0;
+    dirtyheight = 0;
+    fg = 14;
+    bg = 6;
+
+    row = 0;
+    col = 0;
+
+    rows = 24;
+    cols = 40;
+
+    canvas: any = document.getElementById("screen");
+    ctx = this.canvas?.getContext("2d");
+    imgData = this.ctx.getImageData(0, 0, 320, 200);
+    bitmap = this.imgData.data;
+
+    CbmishConsole() {
+        this.clear();
+    }
+
+    public init() {
+        this.out('\r    **** HTML/CSS/TYPESCRIPT ****\r\r');
+        this.out('1GB RAM SYSTEM  1073741824 BYTES FREE\r\r');
+        this.out('READY.\r');    
+    }
+
+    public out(s: string) { 
+        for (var i = 0; i < s.length; ++i)
+            this.outChar(s.charAt(i)); 
+    }
+
+    outChar(s: string) {
+        if (s.length != 1) throw "expected string of exactly one character";
+        const c = s.charCodeAt(0);
+        if (s == '\r') {
+            this.newLine();
+            return;
+        }
+        if (c == 19) {
+            this.home();
+            return;
+        }
+        if (c == 147) {
+            this.clear();
+            return;
+        }
+        if (c < 32 || c >= 127)
+            return;
+
+        const i = this.ascii_to_petscii(c)*8;
+
+        const chardata = [
+            c64_char_rom[i], 
+            c64_char_rom[i+1],
+            c64_char_rom[i+2],
+            c64_char_rom[i+3],
+            c64_char_rom[i+4],
+            c64_char_rom[i+5],
+            c64_char_rom[i+6],
+            c64_char_rom[i+7],
+        ];
+
+        this.drawC64Char(chardata, this.col*8, this.row*8, this.fg);
+        
+        if (++this.col >= this.cols) {
+            this.col = 0;
+            if (++this.row >= this.rows)
+                this.row = 0;
+        }
+    }
+
+    newLine() {
+        if (++this.row >= this.rows)
+            this.row = 0;
+        this.col = 0;
+    }
+
+    clear() {
+        this.ctx.clearRect(0, 0, 320, 200);
+        this.home();
+    }
+
+    home() {
+        this.row = 0;
+        this.col = 0;
+    }
+
+    ascii_to_petscii(c: number): number {
+        if (c < 32) return 32;
+        if (c > 127) return 32;
+        if (c >= 32 && c <= 63) return c;
+        if (c >= 64 && c <= 64+30) return c-64; // @ABC...Z[\]_
+        if (c >= 97 && c <= 96+26) return c-96; // TODO: lowercase
+        return 32;        
+    }
+
+    drawC64Char(chardata: number[], x: number, y: number, fg: number) {
+        let b: number;
+        let r: number;
+      
+        fg = fg & 0xF;
+      
+        for (r = 0; r < 8; ++r) {
+          for (b = 0; b < 8; ++b) {
+            var j = (x+(7-b) + (y+r)*320)*4;
+            if ((chardata[r] & (1 << b)) != 0) {
+              this.bitmap[j + 0] = C64colors[fg][0];
+              this.bitmap[j + 1] = C64colors[fg][1];
+              this.bitmap[j + 2] = C64colors[fg][2];
+              this.bitmap[j + 3] = C64colors[fg][3];
+            }
+            else
+              this.bitmap[j + 3] = 0; // set alpha component to transparent
+          }
+        }
+      
+        requestAnimationFrame(() => this.animationCallback());
+      
+        if (this.dirtywidth == 0 && this.dirtyheight == 0)
+        {
+          this.dirtyx = x;
+          this.dirtyy = y;
+          this.dirtywidth = 8;
+          this.dirtyheight = 8;
+          return;
+        }
+      
+        if (x < this.dirtyx) {
+          this.dirtywidth += this.dirtyx - x;
+          this.dirtyx = x;
+        } else if (x+8 > this.dirtyx + this.dirtywidth)
+          this.dirtywidth += (x+8 - (this.dirtyx + this.dirtywidth));
+        if (y < this.dirtyy) {
+          this.dirtyheight += this.dirtyy - y;
+          this.dirtyy = y;
+        } else if (y+8 > this.dirtyy + this.dirtyheight)
+          this.dirtyheight += (y+8 - (this.dirtyy + this.dirtyheight));
+    }
+      
+    animationCallback() {
+        if (this.dirtywidth == 0 || this.dirtyheight == 0)
+          return;
+        this.ctx.putImageData(this.imgData, 0, 0, this.dirtyx, this.dirtyy, this.dirtywidth, this.dirtyheight);
+        this.dirtyx = 0;
+        this.dirtyy = 0;
+        this.dirtywidth = 0;
+        this.dirtyheight = 0;
+    }      
+
+    foreground(fg: number) {
+        this.fg = fg;
+    }
+
+    background(bg: number) {
+        const canvas = document.getElementsByTagName('canvas');
+        canvas[1].outerHTML = `<canvas class="background background${(bg & 0xF)}"></canvas>`
+    }
+}
