e046ba9b4cafd558ba3f7d09f5b3c90fdcc88c3e
Dave Van Wagner <git@davevw.com>
2022-11-23 08:47:35 -0800

    sample is classic maze

--- sample.ts
+++ sample.ts
@@ -1,4 +1,5 @@
 var cbm = new CbmishConsole();
 cbm.CbmishConsole();
 cbm.foreground(1);
-cbm.repeat(() => cbm.out('Hello World! '), 2);
+cbm.lowercase=false; 
+let id = cbm.repeat(() => cbm.out(cbm.chr$(109.5+Math.random())), 40*18-1, 0);
