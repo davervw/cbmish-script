161188842f630a82c2bd25c834461785446e2e8e
davervw <git@davevw.com>
2023-05-11 21:15:56 -0700

    simpler import/export

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-// Import stylesheets
-import './style.css';
-
 // example of accessing Bible text from JSON via JavaScript
 
-const lib =  require('./bible');
-const bible = lib.bible;
+import { bible } from "./bible.js"
 const books = getBooks();
 
 // MIT LICENSE
